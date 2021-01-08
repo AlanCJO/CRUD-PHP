544407dad40d88f47e7ae36154a5893a2b336862
Alan Oliveira <alancjobm@gmail.com>
2021-01-07 23:08:28 -0300

    alert

--- cadastrar.php
+++ cadastrar.php
@@ -5,10 +5,10 @@ require __DIR__.'/vendor/autoload.php';
 define('TITLE', 'Cadastrar vaga');
 
 use App\Entity\Vaga;
+$obVaga = new Vaga;
 
 if(isset($_POST['titulo'], $_POST['descricao'], $_POST['ativo']))
 {
-    $obVaga = new Vaga;
     $obVaga->titulo     = $_POST['titulo'];
     $obVaga->descricao  = $_POST['descricao'];
     $obVaga->ativo      = $_POST['ativo'];
